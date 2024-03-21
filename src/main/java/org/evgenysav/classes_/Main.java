b6f383709af617984402512a3a65b273f82933be
Evgeny Savonkin <evgenysavonkin@gmail.com>
2024-03-21 18:13:24 +0300

    Done 15 level

--- src/main/java/org/evgenysav/classes_/Main.java
+++ src/main/java/org/evgenysav/classes_/Main.java
@@ -2,33 +2,26 @@ package org.evgenysav.classes_;
 
 import org.evgenysav.classes.Fixer;
 import org.evgenysav.infrastructure.core.impl.ApplicationContext;
-import org.evgenysav.infrastructure.dto.entity.CombustionStartable;
 import org.evgenysav.infrastructure.dto.entity.OrderEntries;
-import org.evgenysav.infrastructure.dto.entity.Vehicles;
 import org.evgenysav.infrastructure.dto.service.*;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Map<Class<?>, Class<?>> interfaceToImplementation = new HashMap<>();
         interfaceToImplementation.put(Fixer.class, MechanicService.class);
 
         ApplicationContext context = new ApplicationContext("org.evgenysav",
                 interfaceToImplementation);
         initAndInsertValuesInDB(context);
-
-        VehicleService vehicleService = context.getObject(VehicleService.class);
-        CombustionEngineService combustionService = context.getObject(CombustionEngineService.class);
-        List<CombustionStartable> combustionEngines = combustionService.getAll();
-        combustionEngines.forEach(System.out::println);
-        List<Vehicles> vehicles = vehicleService.getAll();
-        vehicles.forEach(System.out::println);
+        ScheduledMain scheduledMain = context.getObject(ScheduledMain.class);
+        scheduledMain.moveVehiclesToWorkRoom(context);
+        Thread.sleep(120_000);
     }
 
-    private static void initAndInsertValuesInDB(ApplicationContext context) {
+    static void initAndInsertValuesInDB(ApplicationContext context) {
         context.getObject(VehicleService.class);
         context.getObject(TypesService.class);
         context.getObject(RentsService.class);
